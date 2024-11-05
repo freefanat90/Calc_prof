a7a12c6ce94f16609394ce53a0b495a2bd43bd8d
freefanat90 <freefanat90@gmail.com>
2024-11-05 21:46:18 +0300

    Логика и отладка калькулятора

---
+++ Calculater_logic.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def devide(a,b):
+    if b != 0:
+        return a/b
+    else:
+        return "Деление на ноль!"
